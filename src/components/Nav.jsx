a6d148a481a341191180a1bfb25f5be3cc869ae4
Phanupong <inrongpon.phanupong2@gmail.com>
2022-10-11 13:39:18 +0700

    edit responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -17,13 +17,13 @@ function Nav() {
             </a>
         </div>
         <div className={clicked ? 'showHamMenu' : 'hideHamMenu'}>
-          <ul className="absolute md:static flex flex-col items-center bg-white md:bg-transparent text-[#818498] md:text-white font-semibold py-5 px-5 md:flex-row">
+          <ul className="absolute -right-10 sm:-right-[14rem] md:static flex flex-col items-center bg-white md:bg-transparent text-[#818498] md:text-white font-semibold py-5 px-5 md:flex-row">
               <li className='mx-[40px] my-2'><a href="#">About</a></li>
               <li className='mx-[40px] my-2'><a href="#">Services</a></li>
               <li className='mx-[40px] my-2'><a href="#">Projects</a></li>
               <li className='mx-[40px] my-2 font-fraunces uppercase bg-[#FAD400] text-black md:bg-white rounded-full px-5 py-3 md:hover:text-white md:hover:bg-blue-300'><a href="#">Contact</a></li>
           </ul>
-          <div className='z-10 translate-x-[12.95rem] -translate-y-[0.9rem] w-0 h-0 
+          <div className='z-10 w-0 h-0 absolute -top-3 -right-[2.8rem] sm:-right-[14.3rem]
               border-l-[20px] border-l-transparent
               border-t-[30px] border-t-white
               border-r-[20px] border-r-transparent
